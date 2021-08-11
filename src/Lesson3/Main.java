8036fcfe5de82a34b881d2c06d362ef916abae8f
FunnyRider34Rus <elpablo.volgograd@yandex.ru>
2021-08-12 00:14:33 +0300

    Lesson3

--- src/Lesson3/Main.java
+++ src/Lesson3/Main.java
@@ -7,6 +7,7 @@ public  class Main {
 
     public static void main(String[] args) {
         theGame();
+        guessGame();
     }
 
     static void theGame(){
@@ -34,5 +35,42 @@ public  class Main {
         if (d == 1) {
             theGame();
         }
+    }
+
+    static void guessGame() {
+        Scanner scan = new Scanner(System.in);
+        Random rnd = new Random();
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        int targetWordNum = rnd.nextInt(words.length);
+        char[] tempArray = new char[15];
+        for (int i = 0; i < tempArray.length; i++) {
+            tempArray [i] = '#';
+        }
+
+        System.out.println("Загадано слово из следующего списка:");
+        for (int i = 0; i < words.length; i++) {
+            System.out.print(words [i] + " ");
+        }
+
+        System.out.println("Ваша задача угадать загаданное слово!");
+        boolean equal;
+        System.out.println(words[targetWordNum]);
+        do {
+            System.out.print("\nВведите предполагаемое слово:");
+            String answer = scan.nextLine();
+            equal = words[targetWordNum].equalsIgnoreCase(answer);
+            if (equal) {} else {
+                for (int i = 0; i < answer.length() && i < words[targetWordNum].length(); i++) {
+                    char a = answer.charAt(i);
+                    char b = words[targetWordNum].charAt(i);
+                    if (a == b) tempArray [i] = a;
+                }
+                for (char o : tempArray) {
+                    System.out.print(o);
+                }
+            }
+        } while (!equal);
+        System.out.println();
+        System.out.println("Поздравляю! Вы угадали слово!");
     }
 }
