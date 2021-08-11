b3eb8469146a149ba1afc4910cf01c004035a3bb
FunnyRider34Rus <elpablo.volgograd@yandex.ru>
2021-08-11 21:11:30 +0300

    Lesson3

--- src/Lesson3/Main.java
+++ src/Lesson3/Main.java
@@ -6,41 +6,35 @@ import java.util.Scanner;
 public  class Main {
 
     public static void main(String[] args) {
+        Main main = new Main();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Загадано случайное число от 0 до 9");
         theGame();
+        System.out.println("Повторить игру еще раз? (1 - Да, 0 - Нет)");
+        int d = scan.nextInt();
+        if (d == 1) {
+            theGame();
+        }
     }
 
-    public static boolean theGame(){
+    static void theGame(){
         Scanner scan = new Scanner(System.in);
         Random rnd = new Random();
 
         int targetNum = rnd.nextInt(9);
         for (int i = 3; i > 0; i--) {
-            System.out.println("Загадано случайное число от 0 до 9. У вас есть: " + i + " попыток");
-            System.out.println("Введите предполагаемое число: ");
+            System.out.println(targetNum);
+            System.out.println("У вас осталось: " + i + " попытки(а)");
+            System.out.print("Введите предполагаемое число: ");
             int answer = scan.nextInt();
-            if (answer == targetNum) return true;
+            if (answer > targetNum) {
+                System.out.println("Не угадали! Загаданное число меньше");
+            } else if (answer < targetNum){
+                System.out.println("Не угадали! Загаданное число больше");
+            } else {
+                System.out.println("Поздавляю! Вы угадали!!!");
+                break;
+            }
         }
-        return false;
     }
-
-    void gameLauncher(boolean endGame) {
-        if (endGame) {
-            System.out.println("Поздравляю! Вы отгадали число!");
-        } else {
-            System.out.println("К сожалению, Вы не угадали!");
-        }
-        int d = getNumberFromScanner("Повторить игру еще раз? (1 - Да, 0 - Нет", 0, 1);
-        if (d == 1) theGame();
-        }
-
-    public int getNumberFromScanner(String message, int min, int max) {
-        Scanner scan = new Scanner(System.in);
-        int x;
-        do {
-            System.out.println(message);
-            x = scan.nextInt();
-        } while (x == 0 || x == 1);
-        return x;
-    }
-
 }
