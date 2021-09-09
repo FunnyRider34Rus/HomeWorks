8d5773c8c1fb8aaa28145baf0c1953237e523b0e
FunnyRider34Rus <elpablo.volgograd@yandex.ru>
2021-09-09 09:09:14 +0300

    Exceptions добавлены комментарии

--- src/Lesson9/Main.java
+++ src/Lesson9/Main.java
@@ -1,19 +1,16 @@
 package Lesson9;
 
-
-import java.util.Scanner;
-
 public class Main {
 
     public static String[][] array = new String[4][4];
 
     public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
 
-        String[][] array = {
+        String[][] array = {                                                                                            //создаем тестовый массив
                 {"1", "2", "3", "4"},
                 {"4", "3", "2", "1"},
                 {"9", "8", "7", "6"},
-                {"5", "6", "7", "q"}/*,
+                {"5", "6", "7", "q"},/*
                 {"2", "3", "2", "1"}*/
         };
 
@@ -23,25 +20,31 @@ public class Main {
 
     public static void printArr(String[][] array) throws MyArraySizeException, MyArrayDataException {
 
-        if (array.length > 4) {
+        if (array.length != 4) {                                                                                        //проверяем массив на соответствие его размера
             throw new MyArraySizeException("Превышен размер массива!");
         }
 
-        int[][] arrayInt = new int[4][4];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException("Превышен размер массива!");
+            }
+        }
+
+        int[][] arrayInt = new int[4][4];                                                                               //Создаем буфферный интовый массив
         int sum = 0;
 
         for (int x = 0; x < array.length; x++) {
             for (int y = 0; y < array.length; y++) {
                 try {
-                    arrayInt[x][y] = Integer.parseInt(array[x][y]);
-                    sum += arrayInt[x][y];
-                } catch (NumberFormatException err) {
+                    arrayInt[x][y] = Integer.parseInt(array[x][y]);                                                     //проходимся по массиву
+                    sum += arrayInt[x][y];                                                                              //преобразованные в инт значения
+                } catch (NumberFormatException err) {                                                                   //записываем в буфферный массив
                     throw new MyArrayDataException("Элемент массива с индексом " + x + ", " + y + " имеет не числовой тип данных");
                 }
             }
         }
 
-        System.out.println(sum);
+        System.out.println(sum);                                                                                        //выводим сумму всех значений массива
 
     }
 }
