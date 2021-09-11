50d5c3e147ebdfff8def1544e3d5b42cfb2cb624
FunnyRider34Rus <elpablo.volgograd@yandex.ru>
2021-09-11 11:42:21 +0300

    Collections сделано оформление 10.1

--- src/Lesson10/Main.java
+++ src/Lesson10/Main.java
@@ -5,50 +5,42 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        String[] array = {
-                "ноль",
-                "один",
-                "два",
-                "три",
-                "четыре",
-                "пять",
-                "шесть",
-                "семь",
-                "восемь",
-                "девять",
-                "восемь",
-                "семь",
-                "шесть",
-                "пять"
-        };
+        String[] array = {"ноль", "один", "два", "три", "четыре", "пять", "шесть", "семь", "восемь", "девять", "восемь", "семь", "шесть", "пять"};
+
+        System.out.print("Исходный массив: ");
+        for (String item : array){
+            System.out.print(item + ", ");
+        }
+        System.out.println();
 
         doFirstExample(array);
 
     }
 
     public static void doFirstExample(String array[]){
-            Set<String> array_temp = new HashSet<String>();
+        Set<String> array_temp = new HashSet<String>();
 
-            for (String item : array){
-                array_temp.add(item);
-            }
+        for (String item : array){
+            array_temp.add(item);
+        }
 
-            System.out.println(array_temp);
+        System.out.print("Выводим массив без повторяющихся элементов: ");
+        System.out.println(array_temp);
 
-            HashMap<String, Integer> array_final = new HashMap<>();
-            for (String item : array_temp) {
-                int number = 0;
-                for (int i = 0; i < array.length; i++) {
-                    if (item.equals(array[i])){
-                        number++;
-                    }
-                    array_final.put(item, number);
+        HashMap<String, Integer> array_final = new HashMap<>();
+        for (String item : array_temp) {
+        int number = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (item.equals(array[i])){
+                   number++;
                 }
+                array_final.put(item, number);
             }
+        }
 
-            for (Map.Entry<String, Integer> o : array_final.entrySet()) {
-                System.out.println(o.getKey() + " содержится в количестве " + o.getValue());
-            }
+        for (Map.Entry<String, Integer> o : array_final.entrySet()) {
+            System.out.println(o.getKey() + " содержится в количестве " + o.getValue());
+        }
     }
 }
 
