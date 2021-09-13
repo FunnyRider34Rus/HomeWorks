73d79254429297493c093eaf01fa3e041bcc2750
FunnyRider34Rus <elpablo.volgograd@yandex.ru>
2021-09-13 11:30:25 +0300

    Обобщения реализован 11.1

--- src/Lesson11/Main.java
+++ src/Lesson11/Main.java
@@ -1,7 +1,36 @@
 package Lesson11;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        doFirstExample();
+
+    }
+
+    static void doFirstExample(){
+        Scanner scr = new Scanner(System.in);
+        String[] array = {"1", "2", "3", "4", "5"};
+        System.out.print("Исходный массив: ");
+
+        for (String x : array){
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        System.out.print("Введите номер элемента массива, от 1 до " + array.length + ", который нужно переместить: ");
+        int bufferIndex = scr.nextInt() - 1;
+        System.out.print("Введите номер элемента массива, от 1 до " + array.length + ",куда нужно переместить: ");
+        int bufferIndex2 = scr.nextInt() - 1;
+        String bufferNum = array[bufferIndex2];
+        array[bufferIndex2] = array[bufferIndex];
+        array[bufferIndex] = bufferNum;
+        System.out.println();
+        System.out.print("Измененный массив: ");
+
+        for (String x : array){
+            System.out.print(x + " ");
+        }
     }
 }
