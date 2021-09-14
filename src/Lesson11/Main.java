7d239dcaa3e1b9c168fc202d757cd7a856b318a8
FunnyRider34Rus <elpablo.volgograd@yandex.ru>
2021-09-14 12:43:54 +0300

    Обобщения реализован 11.1 реализован 11.2 с использованием 11.1

--- src/Lesson11/Main.java
+++ src/Lesson11/Main.java
@@ -6,46 +6,35 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //doFirstExample();
-
-        doSecondExample(doFirstExample());
-
-    }
-
-    static String[] doFirstExample(){
         Scanner scr = new Scanner(System.in);
-        String[] array = {"1", "2", "3", "4", "5"};
+        Object[] array = {"1", "2", "3", "4", "5"};
         System.out.print("Исходный массив: ");
 
-        for (String x : array){
+        for (Object x : array) {
             System.out.print(x + " ");
         }
 
         System.out.println();
         System.out.print("Введите номер элемента массива, от 1 до " + array.length + ", который нужно переместить: ");
-        int bufferIndex = scr.nextInt() - 1;
+        int fistIndex = scr.nextInt() - 1;
         System.out.print("Введите номер элемента массива, от 1 до " + array.length + ",куда нужно переместить: ");
-        int bufferIndex2 = scr.nextInt() - 1;
-        String bufferNum = array[bufferIndex2];
-        array[bufferIndex2] = array[bufferIndex];
-        array[bufferIndex] = bufferNum;
-        //System.out.println();
-        System.out.print("Измененный массив: ");
+        int secondIndex = scr.nextInt() - 1;
 
-        for (String x : array){
-            System.out.print(x + " ");
-        }
+        System.out.print("Модифицированный массив: ");
+        System.out.println(toArrayList(exchangeData(array, fistIndex, secondIndex)));
+
+    }
+
+    public static <T> T[] exchangeData(T[] array, int firstIndex, int secondIndex){
+
+        T buffer = array[firstIndex];
+        array[firstIndex] = array[secondIndex];
+        array[secondIndex] = buffer;
+
         return array;
     }
 
-    static void doSecondExample(String [] array){
-        //String[] array = {"1", "2", "3", "4", "5"};
-        ArrayList<String> arrayList = new ArrayList<String>(Arrays.asList(array));
-        System.out.println();
-        System.out.println();
-        System.out.print("Вывод массива ArrayList: ");
-        for(String s : arrayList) {
-            System.out.print(s + " ");
-        }
+    public static <T> ArrayList<T> toArrayList(T[] array){
+        return new ArrayList<>(Arrays.asList(array));
     }
 }
