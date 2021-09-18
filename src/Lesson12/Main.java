01051383c6a17cb67a5a1686179387c23b298b00
FunnyRider34Rus <elpablo.volgograd@yandex.ru>
2021-09-18 20:13:03 +0300

    Многопоточность Введён дополнительный класс

--- src/Lesson12/Main.java
+++ src/Lesson12/Main.java
@@ -1,5 +1,7 @@
 package Lesson12;
 
+import java.util.Arrays;
+
 public class Main {
 
     static final int SIZE = 10000000;
@@ -11,12 +13,12 @@ public class Main {
         float[] array = initArray();
         SingleThread thd0 = new SingleThread(array, 0);
 
-        thd0.start();
-        try{
+        thd0.run();
+        /*try{
             thd0.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
-        }
+        }*/
         System.out.println("Время выполнения задачи в однопоточном режиме: " + (System.currentTimeMillis() - a) + " мс");
 
         a = System.currentTimeMillis();
@@ -36,15 +38,16 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        System.arraycopy(a1, 0, array, 0, HALF);
+        System.arraycopy(a2, 0, array, HALF, HALF);
+
         System.out.println("Время выполнения задачи в двупоточном режиме: " + (System.currentTimeMillis() - a) + " мс");
 
     }
 
     public static float[] initArray (){
         float[] arr = new float[SIZE];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = 1;
-        }
+        Arrays.fill(arr, 1);
         return arr;
     }
 }
